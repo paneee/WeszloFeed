51af6117da3cf58c6e47db40a4e3739c14e0c0de
Jacek Kalamarz <jacek@kalamarz.com>
2018-09-18 09:58:29 -0700

    Minor update

--- feed_itunes.py
+++ feed_itunes.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from feedgen.feed import FeedGenerator
-from urllib import urlencode
 import datetime
 import os
+import time
 from os import listdir
 from os.path import isfile, isdir, join, islink
 from eyed3.id3 import Tag
@@ -23,13 +24,13 @@ def creation_date(path_to_file):
 
 fg = FeedGenerator()
 fg.load_extension('podcast')
-fg.title('Weszlo FM') 
-fg.podcast.itunes_author('Weszlo FM')
-fg.link(href='http://weszlo.com//', rel='alternate')
-fg.subtitle('Nieoficjalny podcast Weszlo FM')
-fg.language('en')
+fg.title(u'Weszło FM') 
+fg.podcast.itunes_author(u'Weszło FM')
+fg.link(href='http://weszlo.fm/', rel='alternate')
+fg.subtitle(u'Nieoficjalny podcast WeszłoFM')
+fg.language('pl')
 fg.copyright('cc-by-Weszlo')
-fg.podcast.itunes_summary('Podcast Weszlo')
+fg.podcast.itunes_summary(u'Podcast WeszłoFM')
 fg.podcast.itunes_owner('Krzysztof Stanowski', 'krzysztof.stanowski@weszlo.com')
 fg.link(href='https://www.simx.mobi/weszlo/', rel='self')
 fg.podcast.itunes_explicit('no')
@@ -54,10 +55,10 @@ for path_folder in only_folders_from_root_path:
         tag.parse(path_files + "/" + path) 
         
         item = fg.add_entry()
-        item.id("https://www.simx.mobi/weszlo/")
+        item.id(u"https://www.simx.mobi/weszlo/" + path_folder + "/" + path)
         item.title(tag.title)
         item.podcast.itunes_summary(tag.artist + " " + tag.title)
-        item.podcast.itunes_subtitle(tag.artist + " " + tag.title)
+        item.podcast.itunes_subtitle(tag.artist)
         item.podcast.itunes_author(tag.artist)
         item.enclosure(u"https://www.simx.mobi/weszlo/" + path_folder + "/" + path, 0, 'audio/mpeg')
         
@@ -66,7 +67,7 @@ for path_folder in only_folders_from_root_path:
         item.podcast.itunes_duration(normTime)
         
         dat = creation_date(path_files + "/" + path) 
-        item.pubdate(str(datetime.datetime.fromtimestamp(dat)) + "-0000")
+        item.pubdate(str(datetime.datetime.fromtimestamp(dat)) + time.strftime("%z"))
         items.append(item)
 
 fg.rss_file('./feed.xml', pretty=True)
